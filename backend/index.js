c656e7c8c87290a3f861cbab9e299f8f386bcd0d
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-01-31 15:40:25 +0000

    installed dependencies on a server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 5000
-
-app.post("/api/login", (req, res) => {
-    const { username, password } = req.body
-})
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const port = 5000;
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+	console.log(`Listening on port ${port}`);
+});
+
+mongoose.connect("mongodb://localhost:27017/jwt", {});
+app.use(
+	cors({
+		origin: ["http://localhost:3000"],
+		method: ["GET", "POST"],
+		credentials: true,
+	})
+);
+
+app.post("/api/login", (req, res) => {
+	const { username, password } = req.body;
+});
+
+app.use(express.json());
